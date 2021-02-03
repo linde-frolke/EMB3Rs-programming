05020d969443e141d70ccc1af0f3412650312ae6
linde_comp <linfr@dtu.dk>
2021-02-03 14:15:41 +0100

    Forgot to attach file to previous commit.

--- test_setup_scripts/test_prototype.py
+++ test_setup_scripts/test_prototype.py
@@ -6,12 +6,14 @@ import numpy as np
 
 # import own modules
 from datastructures.inputstructs import AgentData, MarketSettings
-# from datastructures.resultobject import ResultData
 from market_functions.pool_market import make_pool_market
+from market_functions.p2p_market import make_p2p_market
+
 
 # setup inputs --------------------------------------------
 # make settings object
 settings = MarketSettings(nr_of_hours=12, offer_type="simple", prod_diff="noPref", market_design="pool")
+settings = MarketSettings(nr_of_hours=12, offer_type="simple", prod_diff="noPref", market_design="p2p")
 
 # make agent data object
 agent_ids = ["sérgio", "linde", "tiago1", "tiago2"]
@@ -22,15 +24,15 @@ agent_data = AgentData(settings=settings, name=agent_ids, a_type=agent_types,
                        cost=np.ones((settings.nr_of_h, 4)), util=np.ones((settings.nr_of_h, 4)))
 
 # set model name
-name = "test"
+name = "test_" + str(settings.market_design) + "_" + str(settings.offer_type) + "_" + str(settings.product_diff)
 
-# construct and solve market
+# construct and solve market -----------------------------
 if settings.market_design == "pool":
     result = make_pool_market(name="test", agent_data=agent_data, settings=settings)
 elif settings.market_design == "community":
     print("the " + settings.market_design + " market is not implemented yet")
 elif settings.market_design == "p2p":  # P2P should be here
-    print("the " + settings.market_design + " market is not implemented yet")
+    result = make_p2p_market(name="test", agent_data=agent_data, settings=settings)
 else:
     raise ValueError("settings.market_design has to be in [p2p, community, pool")
 
