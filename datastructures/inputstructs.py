1e349449c96a7623d518937c3924d1d401255ace
linde_comp <linfr@dtu.dk>
2021-02-11 12:53:55 +0100

    Added Network class to inputstructs.py. it will later need to be updated, but for now it can be used to generate the preferences. Also updated the way co2 emissions are added to AgentData object when no co2 emissions are needed.

--- datastructures/inputstructs.py
+++ datastructures/inputstructs.py
@@ -46,7 +46,6 @@ class MarketSettings:
         # TODO add agent locations in grid??
 
 
-
 # agents information --------------------------------------------------------------------------------------------------
 class AgentData:
     """
@@ -90,17 +89,69 @@ class AgentData:
         if settings.product_diff == "co2Emissions":
             self.co2_emission = pd.DataFrame(co2, columns=name)
         else:
-            self.co2_emission = None
-
-        # TODO could add preference matrix here.
-        # def add_preference matrix(self, network):
-        #     blabla
-        #     self.pref_matrix = ..
+            self.co2_emission = pd.DataFrame(np.ones((1, len(name)))*np.nan, columns=name)
 
 
-# # network data --------------------------------------------------------------------------------------------------------
-# TODO
-# class Network:
-#     def __init__(self):
-#         # pipelines on path from Agent 1 to agent 2
-#         #
+# network data ---------------------------------------------------------------------------------------------------------
+class Network:
+    def __init__(self, agent_data, gis_data): # agent_loc,
+        """
+        :param agent_data: AgentData object.
+        :param agent_loc: dictionary mapping agent ids to node numbers
+        :param gis_data: dataframe provided by GIS to us. has columns from/to (tuple), Losses total (W), length (m), total costs
+        :output: a Network object with 2 properties: distance and losses (both n by n np.array). distance[1,3] is the
+        distance from agent 1 to agent 3. has np.inf if cannot reach the agent.
+        """
+        # TODO get this data from GIS module.
+        # # node numbers
+        # self.N = nodes
+        # self.E = edges
+        # define location where agents are
+        # self.loc_a = agent_loc  # TODO map agent id to node numbers
+
+        # define distance and losses between any two agents in a matrix ----------------------------
+        self.distance = np.inf * np.ones((agent_data.nr_of_agents, agent_data.nr_of_agents))
+        self.losses = np.inf * np.ones((agent_data.nr_of_agents, agent_data.nr_of_agents))
+        for i in range(agent_data.nr_of_agents):
+            self.distance[i, i] = 0.0   # distance to self is zero.
+            self.losses[i, i] = 0.0     # losses to self is zero.
+        for row_nr in range(len(gis_data["From/to"].values)):
+            (From, To) = gis_data["From/to"].values[row_nr]
+            self.distance[From, To] = gis_data.Length.iloc[row_nr]
+            self.losses[From, To] = gis_data["Losses total [W]"].iloc[row_nr]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
