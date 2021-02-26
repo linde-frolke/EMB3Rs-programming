c7ac1c434fce954b88e742c211585754ed70e4dc
linde_comp <linfr@dtu.dk>
2021-02-26 15:14:57 +0100

    Restructured the result object. Now, the initialization extracts the needed prima/dual values. Then, class methods can be used to compute the output from those variable values. Another method can be used to plot a market clearing.

--- datastructures/resultobject.py
+++ datastructures/resultobject.py
@@ -15,7 +15,9 @@ class ResultData:
                  agent_data: AgentData, settings: MarketSettings):
         """
         Object to store relevant outputs from a solved market problem.
-        Computes them if needed.
+        Initialization only extracts necessary values from the optimization
+        Functions can be used to compute other quantities or generate plots
+
         :param name: str
         :param prob: cvxpy problem object
         :param cb: ConstraintBuilder used for cvxpy problem
@@ -29,7 +31,6 @@ class ResultData:
         if prob.status in ["infeasible", "unbounded"]:
             self.optimal = False
             raise Warning("problem is not solved to an optimal solution. result object will not contain any info")
-
         else:
             self.optimal = True
             # store values of the optimized variables -------------------------------------------------------
@@ -43,15 +44,11 @@ class ResultData:
                 self.Tnm = [pd.DataFrame(variables[varnames.index("Tnm_" + str(t))].value,
                                          columns=agent_data.agent_name, index=agent_data.agent_name)
                             for t in range(settings.nr_of_h)]
-            
-            # get shadow price, Qoe, for different markets --------------------------------------------------
+
+            # get values related to duals  ----------------------------------------
             if settings.market_design == "pool":
                 self.shadow_price = cb.get_constraint(str_="powerbalance").dual_value
                 self.shadow_price = pd.DataFrame(self.shadow_price, columns=["uniform price"])
-
-                self.QoE = np.nan
-                # raise Warning("QoE not implemented for pool")
-
             elif settings.market_design == "p2p":
                 self.shadow_price = [pd.DataFrame(index=agent_data.agent_name, columns=agent_data.agent_name)
                                      for t in settings.timestamps]
@@ -62,47 +59,57 @@ class ResultData:
                             constr_name = "reciprocity_t" + str(t) + str(i) + str(j)
                             self.shadow_price[t].iloc[i, j] = cb.get_constraint(str_=constr_name).dual_value
                             self.shadow_price[t].iloc[j, i] = - self.shadow_price[t].iloc[i, j]
-
-                # QoE
-                self.QoE = []
-                for t in range(0, settings.nr_of_h):
-                    lambda_j=[]
-                    for a1 in agent_data.agent_name:
-                        for a2 in agent_data.agent_name:
-                            if self.Pn[a1][t] != 0:  # avoid #DIV/0! error
-                                lambda_j.append(agent_data.cost[a1][t]*self.Tnm[t][a1][a2]/self.Pn[a1][t])
-                            if self.Ln[a1][t] != 0:  # avoid #DIV/0! error
-                                lambda_j.append(agent_data.util[a1][t]*self.Tnm[t][a1][a2]/self.Ln[a1][t])
-                    if (max(lambda_j) - min(lambda_j)) != 0:  # avoid #DIV/0! error
-                        self.QoE.append(1 - (st.pstdev(lambda_j) / (max(lambda_j) - min(lambda_j))))
-                    else:
-                        pass
-                # self.qoe = np.average(self.QoE) # we only need it for each hour.
-
             elif settings.market_design == "community":
                 self.shadow_price = "TODO!"
-                self.QoE = np.nan
-                #raise Warning("community shadow price and QoE not implemented yet \n")
 
-            # hourly social welfare an array of length settings.nr_of_h
-            # TODO compute social welfare for each hour!!
-            social_welfare = np.zeros(settings.nr_of_h)
-            self.social_welfare_h = social_welfare
-            # total social welfare
-            self.social_welfare_tot = prob.solution.opt_val
-            # scheduled power injection for each agent, for each time
-            # agree on sign when load / supply
+            # initialize empty slots for uncomputed result quantities
+            self.QoE = None
+            self.social_welfare_h = None
+            self.settlement = None
 
-            # joint market properties
-            #OF not considering penalties; Must be equal to social_welfare except when considering preferences
-            settlement = np.sum(np.sum(np.multiply(agent_data.cost, self.Gn))) - np.sum(np.sum(np.multiply(agent_data.util, self.Ln)))
-            
-            self.joint = pd.DataFrame([self.QoE, settlement, self.social_welfare_tot],
-                                      columns=[settings.market_design],
-                                      index=["QoE", "settlement", "Social Welfare"])
+    # a function to make all relevant output variables
+    def compute_output_quantities(self, settings, agent_data):
+        # get shadow price, Qoe, for different markets --------------------------------------------------
+        if settings.market_design == "pool":
+            self.QoE = np.nan * np.ones(settings.nr_of_h)
+            # raise Warning("QoE not implemented for pool")
+        elif settings.market_design == "p2p":
+            # QoE
+            self.QoE = []
+            for t in range(0, settings.nr_of_h):
+                lambda_j = []
+                for a1 in agent_data.agent_name:
+                    for a2 in agent_data.agent_name:
+                        if self.Pn[a1][t] != 0:  # avoid #DIV/0! error
+                            lambda_j.append(agent_data.cost[a1][t] * self.Tnm[t][a1][a2] / self.Pn[a1][t])
+                        if self.Ln[a1][t] != 0:  # avoid #DIV/0! error
+                            lambda_j.append(agent_data.util[a1][t] * self.Tnm[t][a1][a2] / self.Ln[a1][t])
+                if (max(lambda_j) - min(lambda_j)) != 0:  # avoid #DIV/0! error
+                    self.QoE.append(1 - (st.pstdev(lambda_j) / (max(lambda_j) - min(lambda_j))))
+                else:
+                    pass
+            # self.qoe = np.average(self.QoE) # we only need it for each hour.
+        elif settings.market_design == "community":
+            self.QoE = np.nan * np.ones(settings.nr_of_h)
+            # raise Warning("community shadow price and QoE not implemented yet \n")
+
+        # hourly social welfare an array of length settings.nr_of_h
+        # TODO compute social welfare for each hour!!
+        social_welfare = np.nan * np.ones(settings.nr_of_h)
+        self.social_welfare_h = social_welfare
+        # TODO compute settlement for each hour!!
+        # ObfFun not considering penalties; Must be equal to social_welfare except when considering preferences
+        self.settlement = (np.sum(np.sum(np.multiply(agent_data.cost, self.Gn))) -
+                           np.sum(np.sum(np.multiply(agent_data.util, self.Ln))))
 
     # a function working on the result object, to create plots
-    def plot_market_clearing(self, period, agent_data, outfile):
+    def plot_market_clearing(self, period: int, settings: MarketSettings, agent_data: AgentData, outfile: str):
+        """
+        makes a plot in the file named at path outfile.
+        """
+        if not period <= settings.nr_of_h:
+            raise ValueError("period should be in range(settings.nr_of_h)")
+
         if self.market == "pool":
             data1, data2, yy_gen, yy_load = prep_plot_market_clearing_pool(period, agent_data)
             # Plotting
