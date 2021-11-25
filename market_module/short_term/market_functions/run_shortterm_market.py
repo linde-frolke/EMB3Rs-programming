45bf92792314e18ec47ba71149445f60e9ce664d
linde_comp <linfr@dtu.dk>
2021-11-25 18:58:17 +0100

    function to combine all more pretty!

---
+++ market_module/short_term/market_functions/run_shortterm_market.py
@@ -0,0 +1,102 @@
+"""
+function that makes input structures, runs short term market, makes result object, and returns dictionary outputs
+the inputs are also in the dictionary format
+"""
+import numpy as np
+import pandas as pd
+from ast import literal_eval
+
+# import own modules
+from ...short_term.datastructures.inputstructs import AgentData, MarketSettings, Network
+from ...short_term.market_functions.pool_market import make_pool_market
+from ...short_term.market_functions.p2p_market import make_p2p_market
+from ...short_term.market_functions.community_market import make_community_market
+
+def run_shortterm_market(input_dict):
+    """
+    :param input_dict: could be like this:
+    
+    input_dict = {'sim_name': 'name_of_simulation',
+                  'md': 'pool',  # other options are  'p2p' or 'community'
+                  'nr_of_hours': 12,
+                  'offer_type': 'simple',
+                  'prod_diff': 'noPref',
+                  'network': 'none',
+                  'el_dependent': 'false',
+                  'el_price': 'none',
+                  'agent_ids': ["prosumer_1",
+                            "prosumer_2", "consumer_1", "producer_1"],
+                  'agent_types': ["prosumer", "prosumer", "consumer", "producer"],
+                  'objective': 'none', # objective for community
+                  'community_settings': {'g_peak': 'none', 'g_exp': 'none', 'g_imp': 'none'}, 
+                  'gmin': [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], 
+                           [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0],
+                           [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]], 
+                  'gmax': [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], 
+                           [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0],
+                           [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]], 
+                  'lmin': [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], 
+                           [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0],
+                           [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]], 
+                  'lmax': [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], 
+                           [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0],
+                           [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]], 
+                  'cost': [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], 
+                           [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0],
+                           [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]], 
+                  'util': [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], 
+                           [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0],
+                           [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]], 
+                  'co2': 'none',  # allowed values are 'none' or array of size (nr_of_timesteps, nr_of_agents)
+                  'is_in_community': 'none',  # allowed values are 'none' or boolean array of size (nr_of_agents) 
+                  'block_offer': 'none',  # allowed values are 'none' or TODO ask Sergio what format is 
+                  'is_chp': 'none', # allowed values are 'none' or a list with ids of agents that are CHPs
+                  'chp_pars': 'none',  # a dictionary of dictionaries, including parameters for each agents in is_chp.
+                                                        # {'agent_1' : {'rho' : 1.0, 'r' : 0.15, ...},
+                                                        #  'agent_2' : {'rho' : 0.8, 'r' : 0.10, ...} }
+                  'gis_data': 'none'  # or dictionary of format: 
+                            # {'From/to': [(0, 1), (1, 2), (1, 3)],
+                            #  'Losses total [W]': [22969.228855, 24122.603833, 18138.588662],
+                            #  'Length': [1855.232413, 1989.471069, 1446.688900],
+                            #  'Total_costs': [1.848387e+06, 1.934302e+06, 1.488082e+06]}
+                  }
+    """
+    
+    # create Settings object
+    settings = MarketSettings(nr_of_hours=input_dict['nr_of_hours'], offer_type=input_dict['offer_type'],
+                              prod_diff=input_dict['prod_diff'], market_design=input_dict['md'])
+    
+    if settings.market_design == "community":
+        settings.add_community_settings(input_dict['objective'], 
+                        g_peak=input_dict['community_settings']['g_peak'],
+                        g_exp=input_dict['community_settings']['g_exp'], 
+                        g_imp=input_dict['community_settings']['g_imp'])
+
+    # create AgentData object
+    agent_data = AgentData(settings=settings, name=input_dict['agent_ids'], a_type=input_dict['agent_types'],
+                        gmin=input_dict['gmin'], gmax=input_dict['gmax'],
+                        lmin=input_dict['lmin'], lmax=input_dict['lmax'],
+                        cost=input_dict['cost'], util=input_dict['util'],
+                        co2=input_dict['co2_emissions'], 
+                        is_in_community=None, block_offer=None, is_chp=None, chp_pars=None,default_alpha=10.0 # TODO relate to input_dict
+                        )
+    # create Network object
+    network = Network(agent_data=agent_data, gis_data=input_dict['gis_data'], settings=settings)
+
+    # run market
+    # construct and solve market -----------------------------
+    if settings.market_design == "pool":
+        result = make_pool_market(name="test", agent_data=agent_data, settings=settings)
+    elif settings.market_design == "community":
+        result = make_community_market(name="test_comm", agent_data=agent_data, settings=settings)
+    elif settings.market_design == "p2p":  
+        result = make_p2p_market(name="test", agent_data=agent_data, settings=settings)
+
+    # TODO convert result to dict
+    result_dict = {'linde': 'cool'}
+    return result_dict
+
+
+    
+    
+    
