d92a0efcd901658aac8b25adcef2bc38508e825f
linde_comp <linfr@dtu.dk>
2021-11-25 15:22:49 +0100

    setup all tests for SHORT TERM. Todo:tests for long term

---
+++ market_module/short_term/tests/test_prototype_pool_networkdirections.py
@@ -0,0 +1,84 @@
+"""
+script that makes input datastructures, then applies market functions
+"""
+import numpy as np
+import pandas as pd
+import os
+import sys
+
+# import own modules
+from ...short_term.datastructures.inputstructs import AgentData, MarketSettings, Network
+from ...short_term.market_functions.pool_market import make_pool_market
+from ...short_term.market_functions.p2p_market import make_p2p_market
+
+
+def test_pool_networkdir():
+    md = "pool"
+    # setup inputs --------------------------------------------
+    agent_ids = ["prosumer_1", "prosumer_2", "consumer_1", "producer_1"]
+    agent_types = ["prosumer", "prosumer", "consumer", "producer"]
+
+    settings = MarketSettings(nr_of_hours=12, offer_type="simple", prod_diff="noPref", market_design=md,
+                            network_type="direction")
+    name = "test_" + str(settings.market_design) + "_" + str(settings.offer_type) + "_" + str(settings.product_diff) + \
+        "_" + str(settings.network_type)
+
+    # DATA
+    co2_emissions = np.array([1, 1.1, 0, 1.8])
+    gmin = np.zeros((settings.nr_of_h, len(agent_ids)))
+    gmax = np.array(
+        [[1, 2, 0, 5], [3, 4, 0, 4], [1, 5, 0, 3], [0, 0, 0, 0], [1, 1, 0, 1], [2, 3, 0, 1], [4, 2, 0, 5], [3, 4, 0, 4],
+        [1, 5, 0, 3],
+        [0, 0, 0, 0], [1, 1, 0, 1], [2, 3, 0, 1]])
+    lmin = np.zeros((settings.nr_of_h, len(agent_ids)))
+    lmax = np.array(
+        [[2, 2, 1, 0], [2, 1, 0, 0], [1, 2, 1, 0], [3, 0, 2, 0], [1, 1, 4, 0], [2, 3, 3, 0], [4, 2, 1, 0], [3, 4, 2, 0],
+        [1, 5, 3, 0],
+        [0, 0, 5, 0], [1, 1, 3, 0], [2, 3, 1, 0]])
+    cost = np.array([[24, 25, 45, 30], [31, 24, 0, 24], [18, 19, 0, 32], [0, 0, 0, 0], [20, 25, 0, 18], [25, 31, 0, 19],
+                    [24, 27, 0, 22], [32, 31, 0, 19],
+                    [15, 25, 0, 31], [0, 0, 0, 0], [19, 20, 0, 21], [22, 33, 0, 17]])
+    util = np.array([[40, 42, 35, 25], [45, 50, 40, 0], [55, 36, 45, 0], [44, 34, 43, 0], [34, 44, 55, 0], [29, 33, 45, 0],
+                    [40, 55, 33, 0],
+                    [33, 42, 38, 0], [24, 55, 35, 0], [25, 35, 51, 0], [19, 43, 45, 0], [34, 55, 19, 0]])
+
+    agent_data = AgentData(settings=settings, name=agent_ids, a_type=agent_types,
+                        gmin=gmin, gmax=gmax,
+                        lmin=lmin, lmax=lmax,
+                        cost=cost, util=util,
+                        co2=co2_emissions)
+
+    gis_data = {'From/to': [(0, 1), (1, 2), (1, 3)],
+                'Losses total [W]': [22969.228855, 24122.603833, 18138.588662],
+                'Length': [1855.232413, 1989.471069, 1446.688900],
+                'Total_costs': [1.848387e+06, 1.934302e+06, 1.488082e+06]}
+    gis_data = pd.DataFrame(data=gis_data)
+    network = Network(agent_data=agent_data, gis_data=gis_data, settings=settings)
+
+
+    # construct and solve market -----------------------------
+    if settings.market_design == "pool":
+        result = make_pool_market(name=name, agent_data=agent_data, settings=settings, network=network)
+    elif settings.market_design == "community":
+        print("the " + settings.market_design + " market is not implemented yet")
+    elif settings.market_design == "p2p":  # P2P should be here
+        result = make_p2p_market(name=name, agent_data=agent_data, settings=settings, network=network)
+    else:
+        raise ValueError("settings.market_design has to be in [p2p, community, pool]")
+
+    # MAIN RESULTS
+
+    # Shadow price per hour per node
+    print(result.shadow_price)
+
+    # Energy dispatch
+    print(result.Pn)
+
+    # Settlement
+    print(result.settlement)
+
+    # Social welfare
+    print(result.social_welfare_h)
+
+    # Market Clearing Figure - not implemented for this case
+    print(result.plot_market_clearing(0, settings, agent_data, 'pool_0'))  # user must select the hour
--- market_module/test.py
+++ market_module/test.py
@@ -1,11 +1,21 @@
+from market_module.short_term.tests.test_prototype_p2p_co2 import test_p2p_co2
+from market_module.short_term.tests.test_prototype_p2p_losses import test_p2p_losses
 from .tests.test_lib import defineArguments, processInput
 from .short_term.tests.test_prototype_p2p import test_p2p
+from .short_term.tests.test_prototype_p2p_co2 import test_p2p_co2
+from .short_term.tests.test_prototype_p2p_distance import test_p2p_distance
+from .short_term.tests.test_prototype_p2p_losses import test_p2p_losses
 from .short_term.tests.test_prototype_pool import test_pool
+from .short_term.tests.test_prototype_pool_networkdirections import test_pool_networkdir
 
 # Write Here all the available tests you want to run
 availableTests = {
+    "test:shortterm:pool": test_pool,
+    "test:shortterm.pool:networkdir": test_pool_networkdir,
     "test:shortterm:p2p": test_p2p,
-    "test_short_term1": test_pool
+    "test:shortterm:p2p:co2": test_p2p_co2, 
+    "test:shortterm:p2p:distance": test_p2p_distance, 
+    "test:shortterm:p2p:losses": test_p2p_losses
 }
 
 
