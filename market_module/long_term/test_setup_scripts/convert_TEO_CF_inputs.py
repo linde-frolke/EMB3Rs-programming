75be8ec84bda1a8809ecfae0f1dca0d57e2c1794
linde <linde@pop-os>
2022-02-28 12:21:03 +0100

    Playing around with inverse converting inputs... This is a script that is not used anywhere else

--- market_module/long_term/test_setup_scripts/convert_TEO_CF_inputs.py
+++ market_module/long_term/test_setup_scripts/convert_TEO_CF_inputs.py
@@ -137,3 +137,123 @@ type(all_sources_info[0])
 
 ## how to do util?
 
+
+
+#
+input_dict = {#'sim_name': 'test_pool',
+                  'md': 'pool',  # other options are  'p2p' or 'community'
+                  'nr_of_hours': 12,
+                  'offer_type': 'simple',
+                  'prod_diff': 'noPref',
+                  'network': 'none',
+                  'el_dependent': 'false',  # can be false or true
+                  'el_price': 'none',
+                  'agent_ids': ["prosumer_1",
+                                "prosumer_2", "consumer_1", "producer_1"],
+                  'agent_types': ["prosumer", "prosumer", "consumer", "producer"],
+                  'objective': 'none',  # objective for community
+                  'community_settings': {'g_peak': 'none', 'g_exp': 'none', 'g_imp': 'none'},
+                  'gmin': [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0],
+                           [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0],
+                           [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]],
+                  'gmax': [[1, 2, 0, 5], [3, 4, 0, 4], [1, 5, 0, 3], [0, 0, 0, 0], [1, 1, 0, 1],
+                           [2, 3, 0, 1], [4, 2, 0, 5], [3, 4, 0, 4], [1, 5, 0, 3],
+                           [0, 0, 0, 0], [1, 1, 0, 1], [2, 3, 0, 1]],
+                  'lmin': [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0],
+                           [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0],
+                           [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]],
+                  'lmax': [[2, 2, 1, 0], [2, 1, 0, 0], [1, 2, 1, 0], [3, 0, 2, 0], [1, 1, 4, 0],
+                           [2, 3, 3, 0], [4, 2, 1, 0], [3, 4, 2, 0], [1, 5, 3, 0], [0, 0, 5, 0],
+                           [1, 1, 3, 0], [2, 3, 1, 0]],
+                  'cost': [[24, 25, 45, 30], [31, 24, 0, 24], [18, 19, 0, 32], [0, 0, 0, 0],
+                           [20, 25, 0, 18], [25, 31, 0, 19], [24, 27, 0, 22], [32, 31, 0, 19],
+                           [15, 25, 0, 31], [0, 0, 0, 0], [19, 20, 0, 21], [22, 33, 0, 17]],
+                  'util': [[40, 42, 35, 0], [45, 50, 40, 0], [55, 36, 45, 0], [44, 34, 43, 0],
+                           [34, 44, 55, 0], [29, 33, 45, 0], [40, 55, 33, 0],
+                           [33, 42, 38, 0], [24, 55, 35, 0], [25, 35, 51, 0], [19, 43, 45, 0], [34, 55, 19, 0]],
+                  'co2_emissions': 'none',  # allowed values are 'none' or array of size (nr_of_agents)
+                  'is_in_community': 'none',  # allowed values are 'none' or boolean array of size (nr_of_agents)
+                  'block_offer': 'none',
+                  'is_chp': 'none',  # allowed values are 'none' or a list with ids of agents that are CHPs
+                  'chp_pars': 'none',
+                  'gis_data': 
+                        {'from_to': ['(0, 1)', '(1, 2)', '(1, 3)'],
+                       'losses_total': [22969.228855, 24122.603833, 18138.588662],
+                       'length': [1855.232413, 1989.471069, 1446.688900],
+                       'total_costs': [1.848387e+06, 1.934302e+06, 1.488082e+06]},
+                  'nodes' : ["prosumer_1", "prosumer_2", "consumer_1", "producer_1"],
+                  'edges' : [("producer_1","consumer_1"), ("producer_1","prosumer_1"),
+                             ("prosumer_1","prosumer_2"), ]
+                  }
+
+
+tmp = np.array(input_dict["util"])
+util = {}
+for i in range(tmp.shape[1]):
+    util[input_dict["agent_ids"][i]] = tmp[:, i].tolist()
+
+util.pop("producer_1")
+util
+
+
+fn_gis = "/home/linde/Documents/2019PhD/EMB3Rs/EMB3Rs-programming/market_module/short_term/tests/optimize_network.output.json"
+f = open(fn_gis, "r")
+gis_vals = json.load(f)["output_data"]
+
+gis_vals["res_sources_sinks"]
+
+res_sources_sinks = pd.DataFrame(input_dict["gis_data"]).to_dict("records")
+
+[gis_vals["network_solution_nodes"][i]["osmid"] for i in range(len(gis_vals["network_solution_nodes"]))]
+
+gis_vals["network_solution_nodes"]
+
+[{"osmid" : input_dict["nodes"][i]} for i in range(len(input_dict["nodes"]))]
+
+gis_vals["network_solution_edges"]
+
+[{"from" : input_dict["edges"][i][0], 
+  "to" : input_dict["edges"][i][1]} for i in range(len(input_dict["edges"]))]
+
+
+## CF
+sink_data = np.zeros(365)
+tmp = np.array(input_dict["lmax"])
+lmax = {}
+for i in range(tmp.shape[1]):
+    lmax[input_dict["agent_ids"][i]] = tmp[:, i].tolist()
+
+lmax.pop("producer_1")
+lmax
+
+
+
+strms = [{"sink_id": "aaaa" + str(i), 
+            "streams" : {
+                "stream_id": input_dict["agent_ids"][i],
+                "hourly_stream_capacity" : lmax[input_dict["agent_ids"][i]]
+            } } for i in range(len(input_dict["agent_ids"])-1)]
+
+strms
+
+
+## TEO
+input_data["teo-module"] = {}
+
+tmp = np.array(input_dict["gmax"])
+gmax = {}
+for i in range(tmp.shape[1]):
+    gmax[input_dict["agent_ids"][i]] = tmp[:, i].tolist()
+
+gmax.pop("consumer_1")
+gmax
+
+
+producers = (input_dict["agent_ids"])[:2] + input_dict["agent_ids"][3:4]
+
+AccNewCap = [{'NAME': 'AccumulatedNewCapacity', 
+    'VALUE': max(gmax[producers[i]]),
+    'TECHNOLOGY': producers[i], 
+    'YEAR': '2002'} for i in range(len(producers))]
+
+AccNewCap
