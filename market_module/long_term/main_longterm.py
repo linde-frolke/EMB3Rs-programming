976dd55d078eafb51b359f30d6442ddfb98cef72
Sérgio <antonio.s.faria@inesctec.pt>
2022-08-04 11:19:56 +0100

    Main update

--- market_module/long_term/main_longterm.py
+++ market_module/long_term/main_longterm.py
@@ -1,11 +1,11 @@
 from .market_functions.run_longterm_market import run_longterm_market
 from .report.report_long_term import report_long_term
+from .market_functions.convert_user_and_module_inputs import convert_user_and_module_inputs
+
 
 def main_longterm(in_var):
-
-    longterm_results = run_longterm_market(in_var)
+    mm_input_converted = convert_user_and_module_inputs(in_var)
+    longterm_results = run_longterm_market(mm_input_converted)
     report = report_long_term(longterm_results, in_var['user']['data_profile'])
 
-    return {"results":longterm_results, "report":report}
-
-
+    return {"results": longterm_results, "report": report}
