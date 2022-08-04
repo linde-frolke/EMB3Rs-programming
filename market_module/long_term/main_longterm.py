1066c29388df1c3c95841b0f5297a07861891807
Sérgio <antonio.s.faria@inesctec.pt>
2022-08-04 11:04:34 +0100

    Main report change and asset

--- market_module/long_term/main_longterm.py
+++ market_module/long_term/main_longterm.py
@@ -4,7 +4,7 @@ from .report.report_long_term import report_long_term
 def main_longterm(in_var):
 
     longterm_results = run_longterm_market(in_var)
-    report = report_long_term(longterm_results)
+    report = report_long_term(longterm_results, in_var['user']['data_profile'])
 
     return {"results":longterm_results, "report":report}
 
