8790a1c7a0d24790de93aa393a436fdbfc73ce1b
Sérgio <antonio.s.faria@inesctec.pt>
2022-12-28 15:43:48 +0000

    Fixing QoE in the html report

--- market_module/long_term/main_longterm.py
+++ market_module/long_term/main_longterm.py
@@ -7,6 +7,6 @@ def main_longterm(in_var):
     mm_input_converted = convert_user_and_module_inputs(in_var)
     longterm_results = run_longterm_market(mm_input_converted)
     report = report_long_term(longterm_results, in_var['user']['data_profile'], in_var['user']['fbp_time'],
-                              in_var['user']['fbp_agent'])
+                              in_var['user']['fbp_agent'], in_var['user']['md'])
 
     return {"results": longterm_results, "report": report}
